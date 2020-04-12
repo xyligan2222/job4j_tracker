2f73ab6c5bce7c50096f401386fe39f144b86e18
xyligan2222 <xyligan2222@yandex.ru>
2020-04-12 15:22:50 +0300

    1.7. Состояние объекта.[#261492]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+    public Battery(int energy){
+        this.load = energy;
+    }
+    public void exchange(Battery another){
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(13);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
