c733b572da97fda8fe00dce2f81c3989cefed580
xyligan2222 <xyligan2222@yandex.ru>
2020-04-16 21:43:18 +0300

    5. Аннотация @Override[#261500]

--- src/main/java/ru/job4j/oop/JSONReport.java
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -5,7 +5,5 @@ public class JSONReport extends TextReport {
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() + name + ":" +  name + System.lineSeparator() + body
                 + ":" + body + System.lineSeparator() + "}.";
-
-
     }
 }
