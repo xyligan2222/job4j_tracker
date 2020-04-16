9406dcbce4057c7a37d661355eff097167cdda8e
xyligan2222 <xyligan2222@yandex.ru>
2020-04-16 11:17:19 +0300

    1. Конструктор по умолчанию.[#261497]

---
+++ src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,4 @@
+package ru.job4j.oop;
+
+public class Animal extends Predator {
+}
---
+++ src/main/java/ru/job4j/oop/Tiger.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Tiger {
+    public Tiger() {
+        super();
+        System.out.println("Tiiiiiiiger");
+    }
+
+    public static void main(String[] args) {
+        Predator predator = new Predator();
+        Animal animal = new Animal();
+    }
+}
