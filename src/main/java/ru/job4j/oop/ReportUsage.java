f7827dd9e25d240b30e4a6160d2987252eb5e077
xyligan2222 <xyligan2222@yandex.ru>
2020-04-16 20:30:04 +0300

    4. Переопределение[#261499]

---
+++ src/main/java/ru/job4j/oop/ReportUsage.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class ReportUsage {
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("Report's name", "Report's body");
+        System.out.println(text);
+    }
+}
