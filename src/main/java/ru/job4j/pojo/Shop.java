a2a50fda7ffffd8a6aae997acd4177fb4dc7b879
xyligan2222 <xyligan2222@yandex.ru>
2020-04-17 16:56:25 +0300

    3. Удаление моделей из массива.[#261507]

---
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -0,0 +1,56 @@
+package ru.job4j.pojo;
+
+import java.util.Arrays;
+
+public class Shop {
+    public static void main(String[] args) {
+        Product products[] = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Удаляем значение из ячейки с индексом 1");
+        products[1] = null;
+
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
+        System.out.println();
+        System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+    public Product[] delete (Product[] products, int index){
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products [i + 1];
+        }
+        products[products.length - 1] = null;
+        return  products;
+
+    }
+}
