f7dc2b6de154e6352acc3a3afd1868ca7ea97b0d
xyligan2222 <xyligan2222@yandex.ru>
2020-04-23 13:59:46 +0300

    3. Интерфейс Input[#261454]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class College {
+
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        Student student = freshman;
+        Object object = freshman;
+        System.out.println(freshman);
+        System.out.println(student);
+        System.out.println(object);
+    }
+}
