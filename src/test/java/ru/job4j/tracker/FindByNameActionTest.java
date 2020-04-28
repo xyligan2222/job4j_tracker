7b8401ac782bc1a6a354644b4753a8d1d7180d45
xyligan2222 <xyligan2222@yandex.ru>
2020-04-28 14:14:36 +0300

    9.2. Тесты вывода на консоль в StartUI

---
+++ src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class FindByNameActionTest {
+
+    @Test
+    public void whenCheckOutputName() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream stdOut = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        FindItemByNameAction action = new FindItemByNameAction();
+        action.execute(new StubInput(new String[] {"fix bug"}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "" , System.lineSeparator())
+                .add( "Order № 1"
+                        + " name: " + item.getName()
+                        + " id: " + item.getId()
+                )
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(stdOut);
+    }
+}
