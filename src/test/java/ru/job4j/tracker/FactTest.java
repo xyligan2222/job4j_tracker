1d66048a02b08b5239e0972ec2fbe18891a8301e
xyligan2222 <xyligan2222@yandex.ru>
2020-04-29 13:39:22 +0300

    0.3. Тестирование исключений с junit

---
+++ src/test/java/ru/job4j/tracker/FactTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.ex.Fact;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class FactTest {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenFactLessNull() {
+        int rsl = Fact.calc(-6);
+    }
+    @Test
+    public void whenFactPositive() {
+        int rsl = Fact.calc(5);
+        assertThat(rsl,is(120));
+    }
+}
