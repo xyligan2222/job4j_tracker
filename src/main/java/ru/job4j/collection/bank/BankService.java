b8529cc39a7e778d6d686a36157b2881e0a92b3d
xyligan2222 <xyligan2222@yandex.ru>
2020-05-12 21:02:13 +0300

    3. Банковские переводы. [#261441]

---
+++ src/main/java/ru/job4j/collection/bank/BankService.java
@@ -0,0 +1,69 @@
+package ru.job4j.collection.bank;
+
+
+
+import java.util.*;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+       User user = findByPassport(passport);
+       if (user != null && users.get(user).equals(passport)) {
+           users.get(user).add(account);
+       }
+
+
+    }
+
+    public User findByPassport(String passport) {
+        User rsl = null;
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                rsl = user;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        Account rsl = null;
+        User user = findByPassport(passport);
+        List<Account> accounts = new ArrayList<>();
+        if (accounts.size() > 0 && user != null) {
+            for (Account account : accounts) {
+                if (account.getRequisite().equals(requisite)) {
+                 rsl = account;
+                 break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String dеstRequisite, double amount) {
+        boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, dеstRequisite);
+        if (srcAccount.getBalance() - amount >= 0 && srcAccount != null && destAccount != null) {
+           srcAccount.setBalance(srcAccount.getBalance() - amount);
+           destAccount.setBalance(destAccount.getBalance() + amount);
+           rsl = true;
+        }
+        return rsl;
+    }
+    public static void main(String[] args) {
+        List<Account> accounts = new ArrayList<>();
+        String requisite = "3fdsbb9";
+        accounts.add(new Account("3fdsbb9", 140));
+        int index = accounts.indexOf(new Account(requisite, -1));
+        Account find = accounts.get(index);
+        System.out.println(find.getRequisite() + " -> " + find.getBalance());
+    }
+}
