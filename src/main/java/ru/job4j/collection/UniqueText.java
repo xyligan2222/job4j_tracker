d9a96cc3ea5a3bd1d8fcb8341fc82f664602bc8d
xyligan2222 <xyligan2222@yandex.ru>
2020-05-12 10:07:26 +0300

    2. Идентичные тексты. [#261445]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String origins: origin) {
+            check.add(origins);
+        }
+        for (String texts: text) {
+            if (check.contains(texts) == false) {
+                rsl = false;
+            }
+        }
+
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/tracker/UniqueTextTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.collection.UniqueText;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UniqueTextTest {
+
+    @Test
+    public void isEquals() {
+        String origin = "My cat eats a mouse";
+        String text = "My cat eats a mouse";
+        assertThat(UniqueText.isEquals(origin, text), is(true));
+    }
+
+    @Test
+    public void isNotEquals() {
+        String origin = "My cat eats a mouse";
+        String text = "A mouse is eaten by a cat";
+        assertThat(UniqueText.isEquals(origin, text), is(false));
+    }
+}
