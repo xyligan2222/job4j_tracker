9d2acb09057ec30e773994f032b4ff200796a0d9
xyligan2222 <xyligan2222@yandex.ru>
2020-05-16 15:06:32 +0300

    1. Организовать сортировку User [#261427]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,39 @@
+package ru.job4j.collection;
+
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int cmp = this.name.compareTo(o.name);
+        if (cmp == 0 && this.age - o.age > 0) {
+            return 1;
+        } else if (cmp == 0 && this.age - o.age < 0) {
+            return -1;
+        }
+        return cmp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
