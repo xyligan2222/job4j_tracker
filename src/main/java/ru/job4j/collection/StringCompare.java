ee7f582d4f1c5e17027fab1ed585758bde109600
xyligan2222 <xyligan2222@yandex.ru>
2020-05-17 21:39:26 +0300

    3. Компаратор для строк [#261425] add тернарное сравнение

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -7,25 +7,16 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         char x, y;
         int result = 0;
-        int size = left.length() - right.length();
-        if (size > 0) {
-            size = right.length();
-        } else {
-            size = left.length();
-        }
+        int size = Math.min(left.length(), right.length());
+
             for (int i = 0; i < size ; i++) {
                 x = left.charAt(i);
                 y = right.charAt(i);
                 result = Character.compare( x, y);
                 if (result != 0) {
-                    return result;
+                    break;
                 }
-                if (i == size - 1 && result == 0 && left.length() > right.length()) {
-                    return result = 1;
-                }
-                if (i == size - 1 && result == 0 && left.length() < right.length()) {
-                    return result = -1;
-                }
+                result = result == 0 ? Integer.compare(left.length(),right.length()) : result;
             }
 
 
