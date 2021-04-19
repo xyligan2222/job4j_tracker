5fdffa7c2d709776a7941e48b918d9356d0e21d6
xyligan2222 <xyligan2222@yandex.ru>
2021-04-19 16:04:13 +0300

    0.1. Генерация схемы через Hibernate [#368688]

--- src/main/java/ru/job4j/tracker/HibernateRun.java
+++ src/main/java/ru/job4j/tracker/HibernateRun.java
@@ -6,6 +6,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public class HibernateRun {
@@ -14,11 +15,13 @@ public class HibernateRun {
                 .configure().build();
         try {
             SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-            Item item = create(new Item("Learn Hibernate"), sf);
+            Item item = create(new Item("Petr I", "Normalno", new Timestamp(System.currentTimeMillis())), sf);
+            Item item2 = create(new Item("Petr II", "Tsar", new Timestamp(System.currentTimeMillis())), sf);
+            Item item3 = create(new Item("Petr III", "God", new Timestamp(System.currentTimeMillis())), sf);
             System.out.println(item);
-            item.setName("Learn Hibernate 5.");
-            update(item, sf);
-            System.out.println(item);
+            //item.setName("Learn Hibernate 5.");
+            //update(item, sf);
+            //System.out.println(item);
             Item rsl = findById(item.getId(), sf);
             System.out.println(rsl);
             //delete(rsl.getId(), sf);
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import javax.persistence.*;
 
+import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
@@ -12,10 +13,17 @@ public class Item implements Comparable <Item> {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String description;
+    private Timestamp created;
 
     public Item() {
     }
 
+    public Item(String name, String description, Timestamp created) {
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
 
     public Item(String name) {
         this.name = name;
@@ -26,6 +34,21 @@ public class Item implements Comparable <Item> {
     }
 
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
 
     public void setId(Integer id) {
         this.id = id;
@@ -39,28 +62,33 @@ public class Item implements Comparable <Item> {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return id.equals(item.id) && name.equals(item.name);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
 
     @Override
     public int compareTo(Item o) {
         return 0;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id.equals(item.id) && name.equals(item.name) && Objects.equals(description, item.description) && created.equals(item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, created);
+    }
+
     @Override
     public String toString() {
         return "Item{" +
-                "id='" + id + '\'' +
+                "id=" + id +
                 ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", created=" + created +
                 '}';
     }
 }
